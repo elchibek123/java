0df08ee45827c3989b375e54bbdb364d6ba2f0fe
elchibek123 <elchibek.kamalov@gmail.com>
2024-09-01 18:48:35 -0400

    added inheritance

---
+++ inheritance/ConstructorsInInheritance.java
@@ -0,0 +1,26 @@
+class Parent {
+    public Parent()
+    {
+        System.out.println("Parent Constrcutor");
+    } 
+}
+
+class Child extends Parent {
+    public Child()
+    {
+        System.out.println("Child Constructor");
+    }
+}
+
+class GrandChild extends Child {
+    public GrandChild()
+    {
+        System.out.println("Grand Child Constructor");
+    }
+}
+public class ConstructorsInInheritance {
+    public static void main(String[] args) 
+    {
+        GrandChild c=new GrandChild();        
+    }
+}
